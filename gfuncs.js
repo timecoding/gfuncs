fa876bfe07bf06678734635f4cd78eed986952b1
timecoding <timecoding1@gmail.com>
2018-12-21 21:52:28 +0200

    Added function clean

--- gfuncs.js
+++ gfuncs.js
@@ -12,4 +12,36 @@ function time() {
 	return "[" + new Date().toLocaleString(locale, options) + "]";
 }
 
-module.exports = { time: time };
+const clean = function clr(path, msg) {
+	let files;
+	try {
+		fs.accessSync(path);
+		files = fs.readdirSync(path);
+	} catch (e) {
+		console.log(`${time()} No such file or directory ${process.cwd()}/${path}`);
+		return;
+	}
+
+	if (files.length > 0) {
+		files.forEach(function(item) {
+			let stats = fs.statSync(path + item);
+
+			if (stats.isFile()) {
+				fs.unlinkSync(path + item);
+			} else if (stats.isDirectory()) {
+				if (fs.readdirSync(path + item).length > 0) {
+					clr(path + item + '/');
+					fs.rmdirSync(path + item);
+				} else {
+					fs.rmdirSync(path + item);
+				}
+			}
+		});
+		if (msg)
+			console.log(msg);
+	} else {
+		console.log(`${time()} Nothing to clean	in directory ${process.cwd()}/${path}`);
+	}
+}
+
+module.exports = { clean: clean, time: time };
